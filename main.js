ecd253f244e7dae0bd122e6db25942744a058a76
mathisrvfan <98718993+mathisrvfan@users.noreply.github.com>
2023-03-07 19:48:45 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 on = OmegaNum
 number = 1
+let gameinterval
 function increaseNumber() {
   number = OmegaNum.add(1,number)
   print(number)
 }
-increaseNumber()
+gameinterval = setInterval(increaseNumber,33)
